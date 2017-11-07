f400863a5bc477108236f8df5da6c79a0782d5be
Renzo <20145096@student.hust.edu.vn>
2017-11-07 18:09:09 +0700

    update getListProduct

--- moki/app/router/product/api-getListProduct.js
+++ moki/app/router/product/api-getListProduct.js
@@ -1,24 +1,60 @@
 
-module.exports = function(app,product){
+module.exports = function (app, product) {
     app.post('/get_list_product', (req, res) => {
-      product.find({"category.category_id": req.body.category_id}, (err, rs) => {
-         let result = null;
-         console.log("a");
-          if (rs.length != 0) {
-            
-             result = {
-              code :1000,
-              message :"OK",
-              data :rs,
-            }  
+        product.find({ "category.category_id": req.body.category_id }, (err, rs) => {
+            var last_id;
+            var index = parseInt(2);
+            var count = 2;
+            var listProduct = [];
+
+
+
+
+            if (rs.length != 0) {
+
+                //1   i<3   
+                for (var i = index; i < index + count; i++) {
+
+                    listProduct[i - index] = rs[i];
+                    if (i == (index + count - 1)) {
+                        last_id = listProduct[i - index].id_product;
+                    }
+
+                };
+
+
+
+
+
+                let result = null;
+                result = {
+                    code: 1000,
+                    message: "OK",
+                    data: listProduct,
+                    last_id: last_id,
+                }
+                return res.json(result)
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+            else {
+                result = {
+                    code: 9992,
+                    message: "Product is not existed.",
+                }
+            }
             return res.json(result)
-          } else{
-            result = {
-              code :9992,
-              message :"Product is not existed.",            
-            } 
-          }
-          return res.json(result)
         })
-      });
+    });
 }
