dab56e496dc15aefa5f42f25914e10e7c417e8e0
truongcoino1 <truongbk227@gmail.com>
2017-11-08 16:12:04 +0700

    â

--- moki/app/router/product/api-getListProduct.js
+++ moki/app/router/product/api-getListProduct.js
@@ -1,53 +1,67 @@
-
 module.exports = function (app, product) {
     app.post('/get_list_product', (req, res) => {
-        product.find({ "category.category_id": req.body.category_id }, (err, rs) => {
-            var last_id;
-            var index = parseInt(2);
-            var count = 2;
-            var listProduct = [];
-
-
-
+        product.find({ "category.category_id": req.body.category_id }, (err, rs) => { 
 
             if (rs.length != 0) {
-
-                //1   i<3   
-                for (var i = index; i < index + count; i++) {
-
-                    listProduct[i - index] = rs[i];
-                    if (i == (index + count - 1)) {
-                        last_id = listProduct[i - index].id_product;
+             if(rs.length > 20){
+                   if(req.body.index === 0){
+                    console.log("vao")
+                    let listProduct =[];
+                    let lastID = null;
+                    for(var i =0; i < 20; i++){
+                        listProduct.push(rs[i]);
+                        lastID = rs[i].id_product;
                     }
 
-                };
-
-
-
-
-
-                let result = null;
-                result = {
+               let result = {
                     code: 1000,
                     message: "OK",
                     data: listProduct,
-                    last_id: last_id,
+                    last_id: lastID,
                 }
                 return res.json(result)
 
+                } else{
+                    let count =0;
+                    let listProduct =[];
+                    let lastID = null;
+                    for(var i =0; i < rs.length; i++){
+                        if(rs[i].id_product === req.body.last_id){
+                            count = i+1;
+                        }
+                    }
+                    if(rs.length -count >= 20){
+                        for(var i =count; i < count +20; i++){
+                        listProduct.push(rs[i]);
+                        lastID = rs[i].id_product;
+                        }
+                    } else{
+                        for(var i =count; i < rs.length; i++){
+                        listProduct.push(rs[i]);
+                        lastID = rs[i].id_product;
+                        }
+                    }
 
+                    let result = {
+                    code: 1000,
+                    message: "OK",
+                    data: listProduct,
+                    last_id: lastID,
+                }
+                return res.json(result)
+                }
 
-
-
-
-
-
+            } else{
+                 let result = {
+                    code: 1000,
+                    message: "OK",
+                    data: rs,
+                    last_id: "Đã hết sản phẩm",
+                }
+                return res.json(result)
             }
 
-
-
-
-
+            }
             else {
                 result = {
                     code: 9992,
