13b0f15b282713ffc1abd0f8a102adadef0f6f71
truongcoino1 <truongbk227@gmail.com>
2017-11-17 23:46:51 +0700

    a

--- moki/app/router/profile/api-profile.js
+++ moki/app/router/profile/api-profile.js
@@ -7,4 +7,5 @@ module.exports = function (app, db) {
  require('./api-get-infor')(app,profile);
  require('./api-set-infor')(app,profile);
  require('./product/api-get-list-product-by-profile')(app,profile,product);
+ require('./api-update-notification')(app,profile);
 };
--- moki/app/router/profile/api-update-notification.js
+++ moki/app/router/profile/api-update-notification.js
@@ -6,19 +6,25 @@ module.exports = function (app, profile) {
         if (rs.length != 0) {
         let list_notification = rs[0].list_notification;
         for(var i =0; i < list_notification.length; i++){
-            if(list_notification[i].id === req.body.id_notification){
-                rs[0].list_notification[i].view = "1";
+            if(list_notification[i].id.toString() === req.body.id_notification){
+              let noti = list_notification[i];
+               rs[0].list_notification.splice(i,1);
+              noti.view ='1';
+               rs[0].list_notification.push(noti);
+               rs[0].list_notification.sort(function(a, b){return a.id-b.id});
             }
         }
-        rs[0].save((e,r)=>{
-
-        });
-          let result = {
-            code: 1000,
-            message: "OK",
-            data: rs[0],
+        rs[0].save(function(err, post){
+          if(!err){
+            let result = {
+              code: 1000,
+              message: "OK",
+              data: rs[0],
+            }
+            return res.json(result);
           }
-          return res.json(result);
+        });
+          
         }
         else {
           let result = {
